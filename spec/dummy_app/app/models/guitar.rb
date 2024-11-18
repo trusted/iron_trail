e06868793777365725816c6d23506931bb67b8f8
André Diego Piske <andrepiske@gmail.com>
2024-11-18 18:14:19 -0300

    get join and eager_load to work

--- spec/dummy_app/app/models/guitar.rb
+++ spec/dummy_app/app/models/guitar.rb
@@ -17,6 +17,16 @@ class IronTrailFakeAssoc < ActiveRecord::Associations::HasManyAssociation
 
     scope
   end
+
+  # parent method implementation at:
+  # https://github.com/rails/rails/blob/v7.2.2/activerecord/lib/active_record/associations/association.rb#L226-L250
+  def find_target
+    scope.to_a
+  end
+end
+
+# TODO: remove
+module IronTrailExtensions
 end
 
 class IronTrailReflection < ActiveRecord::Reflection::AssociationReflection
@@ -24,6 +34,47 @@ class IronTrailReflection < ActiveRecord::Reflection::AssociationReflection
 
   def association_class
     IronTrailFakeAssoc
+  end
+
+  # TODO: remove
+  def extensions
+    Array(options[:extend]) + [IronTrailExtensions]
+  end
+
+  def join_scope(table, foreign_table, foreign_klass)
+    predicate_builder = predicate_builder(table)
+    scope_chain_items = join_scopes(table, predicate_builder)
+    klass_scope       = klass_join_scope(table, predicate_builder)
+
+    # TODO: what is this all about?
+    # scope_chain_items.inject(klass_scope, &:merge!)
+
+    foreign_key_column_names = Array(join_foreign_key)
+    # TODO: trigger exception if key is composite? (foreign_key_column_names.length > 1)
+    foreign_key_column_name = foreign_key_column_names.first
+
+    # record_id is always of type text, but the foreign table primary key
+    # could be anything (int, uuid, ...)
+    foreign_value = Arel::Nodes::NamedFunction.new(
+      'CAST',
+      [
+        Arel::Nodes::As.new(
+          foreign_table[foreign_key_column_name],
+          Arel::Nodes::SqlLiteral.new('text')
+        ),
+      ]
+    )
+
+    klass_scope.where!(
+      table['record_id']
+        .eq(foreign_value)
+        .and(
+          table['record_table']
+          .eq(foreign_table.name)
+        )
+    )
+
+    klass_scope
   end
 end
 
--- spec/services/people_manager_spec.rb
+++ spec/services/people_manager_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe PeopleManager do
       results = ActiveRecord::Base.connection.execute("select * from irontrail_changes WHERE record_table='people' AND record_id=#{person.id}::text").to_a
       expect(results.length).to be 1
 
-      record_new = JSON.parse(results.first['new_record'])
+      record_new = JSON.parse(results.first['rec_new'])
       expect(record_new).to eq(person.as_json)
     end
 
@@ -46,7 +46,7 @@ RSpec.describe PeopleManager do
         people.each do |person|
           res = ActiveRecord::Base.connection.execute(<<~SQL).to_a
             SELECT * FROM irontrail_changes WHERE
-            record_table='guitars' AND new_record->>'person_id'='#{person.id}'
+            record_table='guitars' AND rec_new->>'person_id'='#{person.id}'
             ORDER BY id ASC
           SQL
 
@@ -54,7 +54,7 @@ RSpec.describe PeopleManager do
             "#{n} #{person.full_name}"
           end
           actual_names = res.map do |change_record|
-            new_record = JSON.parse(change_record['new_record'])
+            new_record = JSON.parse(change_record['rec_new'])
             new_record['description']
           end
 
