5cbac2d0ff76205955d76261b039c44fce1b8734
André Diego Piske <andrepiske@gmail.com>
2024-11-26 13:38:17 -0300

    implement changes from and add tests

--- lib/iron_trail/change_model_concern.rb
+++ lib/iron_trail/change_model_concern.rb
@@ -6,27 +6,27 @@ module IronTrail
 
     module ClassMethods
       def where_object_changes_to(args = {})
+        _where_object_changes(1, args)
+      end
+
+      def where_object_changes_from(args = {})
+        _where_object_changes(0, args)
+      end
+
+      private
+
+      def _where_object_changes(ary_index, args)
         scope = all
 
         args.each do |col_name, value|
           scope.where!(
-            ::Arel::Nodes::SqlLiteral.new("rec_delta->#{connection.quote col_name}->>1").eq(
+            ::Arel::Nodes::SqlLiteral.new("rec_delta->#{connection.quote col_name}->>#{Integer(ary_index)}").eq(
               ::Arel::Nodes::BindParam.new(value)
             )
           )
         end
 
         scope
-        # where_object_changes(0, args)
-      end
-
-      def where_object_changes_from(args = {})
-        where_object_changes(0, args)
-      end
-
-      private
-
-      def where_object_changes(ary_index, args)
       end
     end
   end
