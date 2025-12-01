5a8e8f1a44f650bf90bd5b632d708e50c323c3f2
Felipe Marchi <felipe.marchi@trustedhealth.com>
2025-12-01 15:11:42 -0300

    Fix migration generator

--- lib/generators/iron_trail/migration_generator.rb
+++ lib/generators/iron_trail/migration_generator.rb
@@ -13,20 +13,33 @@ module IronTrail
     def create_changes_migration_file
       migration_dir = File.expand_path("db/migrate")
 
-      migration_template(
-        "create_irontrail_changes.rb.erb",
-        "db/migrate/create_irontrail_changes.rb"
-      )
+      unless self.class.migration_exists?(migration_dir, "create_irontrail_changes")
+        migration_template(
+          "create_irontrail_changes.rb.erb",
+          "db/migrate/create_irontrail_changes.rb"
+        )
+      end
 
-      migration_template(
-        "create_irontrail_support_tables.rb.erb",
-        "db/migrate/create_irontrail_support_tables.rb"
-      )
+      unless self.class.migration_exists?(migration_dir, "create_irontrail_support_tables")
+        migration_template(
+          "create_irontrail_support_tables.rb.erb",
+          "db/migrate/create_irontrail_support_tables.rb"
+        )
+      end
 
-      migration_template(
-        "create_irontrail_trigger_function.rb.erb",
-        "db/migrate/create_irontrail_trigger_function.rb"
-      )
+      unless self.class.migration_exists?(migration_dir, "create_irontrail_trigger_function")
+        migration_template(
+          "create_irontrail_trigger_function.rb.erb",
+          "db/migrate/create_irontrail_trigger_function.rb"
+        )
+      end
+
+      unless self.class.migration_exists?(migration_dir, "create_irontrail_change_callbacks")
+        migration_template(
+          "create_irontrail_change_callbacks.rb.erb",
+          "db/migrate/create_irontrail_change_callbacks.rb"
+        )
+      end
     end
 
     def self.next_migration_number(dirname)
